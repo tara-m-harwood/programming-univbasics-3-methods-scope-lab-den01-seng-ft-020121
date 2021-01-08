4cf4b3b5e63ffd6c94f7901fb50d7b557d6d5398
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-08 22:12:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,9 +4,13 @@ def mario
   phrase
 end
 
-
-
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+  status
+end
+
+def link
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
   status
